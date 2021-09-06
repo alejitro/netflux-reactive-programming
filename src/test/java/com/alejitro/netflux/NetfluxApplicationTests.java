e3fd63c10a2c92dd3ee8ae9ab17d0fe2fbddd305
amartinez_tul <alejandro.martinez@soytul.com>
2021-09-06 16:31:03 -0500

    first commit

---
+++ src/test/java/com/alejitro/netflux/NetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejitro.netflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
